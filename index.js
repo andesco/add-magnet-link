c29421275aaebd09da497c45db845bc09665ee39
Andrew E. <andrewe@icloud.com>
2025-10-20 15:48:52 -0400

    update <title> occurances

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function getResultPage(result) {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>${isSuccess ? 'Success' : 'Warning'} - qBittorrent Worker</title>
+    <title>Add Magnet Link: ${isSuccess ? 'Success' : 'Warning'}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <style>
@@ -208,7 +208,7 @@ function getResultPage(result) {
  <html>
  <head>
      <meta charset="UTF-8">
-     <title>Add Magnet - qBittorrent Worker</title>
+     <title>Add Magnet Link</title>
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
      <style>
@@ -257,7 +257,7 @@ function getAuthPage() {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Authenticate - qBittorrent Worker</title>
+    <title>Add Magnet Link: Authenticate</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <style>
